5092a73e3b4e5937dcd373b3960814126593bc14
Atul Goyal <goyalatul278@gmail.com>
2022-06-09 12:56:27 +0530

    first commit

---
+++ bst.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#pragma once
+namespace first_space {
+    void func(){
+        std::cout<<"inside the first one";
+    }
+}
+
+namespace second_one{
+    void func(){
+        std::cout<<"inside the second one";
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include "bst.cpp"
+
+int main() {
+    first_space::func();
+    std::cout<<'\n';
+    second_one::func();
+
+
+    return 0;
+}
